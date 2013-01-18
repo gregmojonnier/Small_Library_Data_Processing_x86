fb3c8ccb0ee8c22655aefccd6c3e6ae73f15f7aa
Greg Mojonnier <gregmojonnier@gmail.com>
2013-01-18 14:54:04 -0500

    Finished up new_item function, simplified new_patron by having the patron* start/end in %eax

---
+++ new_item.s
@@ -0,0 +1,55 @@
+// @author Greg Mojonnier
+.data
+
+.bss
+
+.text
+.globl new_item
+new_item:
+	enter $0, $0
+// uint16_t cid1, uint16_t cid2, char* author, char* title, uint8_t copies
+
+// Allocate 24 bytes(size of Item struct)
+	movl $24, %eax
+	pushl %eax
+	call allocate
+	add $4, %esp
+
+// Return Null if any allocations fail
+	cmp $0x0, %eax
+	je ALLOCATION_FAILED	
+
+// Zero out the following, Next pointer(byte 0)
+// first patron checked out item pointer(byte 4)
+// Count of item available for check out(byte 21)
+	movl $0x0, (%eax)
+	movl $0x0, 4(%eax)
+	movb $0x0, 21(%eax)
+
+// Move first arg, lefthand cid into(byte 16)
+	movw 8(%ebp), %dx
+	movw %dx, 16(%eax)
+
+// Move second arg, righthand cid into(byte 18)
+	movw 12(%ebp), %dx
+	movw %dx, 18(%eax)
+
+// Move third arg, char* author  into(byte 8)
+	movl 16(%ebp), %edx
+	movl %edx, 8(%eax)
+
+// Move fourth arg, char* title  into(byte 12)
+	movl 20(%ebp), %edx
+	movl %edx, 12(%eax)
+
+// Move fifth arg, uint8_t num copies  into(byte 20)
+	movb 24(%ebp), %dl
+	movb %dl, 20(%eax)
+
+EPILOGUE:
+	leave
+	ret	
+
+ALLOCATION_FAILED:
+	movl $0x0, %eax
+	jmp EPILOGUE
--- new_patron.s
+++ new_patron.s
@@ -9,7 +9,7 @@ new_patron:
 	enter $0, $0
 // char pid1, uint16_t pid2, char* name
 
-// Allocate 16 bytes(size of Patron size)
+// Allocate 16 bytes(size of Patron struct)
 	movl $16, %eax
 	pushl %eax
 	call allocate
@@ -19,31 +19,25 @@ new_patron:
 	cmp $0x0, %eax
 	je ALLOCATION_FAILED	
 
-// Move our allocated bytes into %ecx
-	movl %eax, %ecx
-	
 // Zero out the following, Next pointer(byte 0)
 // first item checked out pointer(byte 4)
 // Count of items checked out(byte 15)
-	movl $0x0, (%ecx)
-	movl $0x0, 4(%ecx)
-	movb $0x0, 15(%ecx)
+	movl $0x0, (%eax)
+	movl $0x0, 4(%eax)
+	movb $0x0, 15(%eax)
 
 // Move first arg, char portion pid into(byte 14)
 	movb 8(%ebp), %dl
-	movb %dl, 14(%ecx)
+	movb %dl, 14(%eax)
 	
 // Move second arg, numeric portion pid into(byte 12)
 	movw 12(%ebp), %dx
-	movw %dx, 12(%ecx)
+	movw %dx, 12(%eax)
 
 // Make byte 8 of our patron struct point to same char
 // as supplied 3rd arg char* name
 	movl 16(%ebp), %edx
-	movl %edx, 8(%ecx)
-
-// Return pointer to allocated patron struct
-	movl %ecx, %eax
+	movl %edx, 8(%eax)
 
 EPILOGUE:
 	leave
