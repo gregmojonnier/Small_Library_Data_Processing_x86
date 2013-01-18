69c8306f226b606ebd32b770940a5ef42d7436cf
Greg Mojonnier <gregmojonnier@gmail.com>
2013-01-18 14:08:11 -0500

    Cleaned up new_patron was doing it correctly after all

--- new_patron.s
+++ new_patron.s
@@ -37,46 +37,8 @@ new_patron:
 	movw 12(%ebp), %dx
 	movw %dx, 12(%ecx)
 
-// Store 3rd arg(char * name)
-	//movl 16(%ebp), %edx
-
-// Save our saved patron struct location(16 bytes) on stack
-// and then find length of char* name
-	//pushl %ecx
-	//pushl %edx
-	//call strlen
-	//add $4, %esp
-
-// Allocate space for char* name
-	//add $1, %eax
-	//pushl %eax
-	//call allocate
-	//add $4, %esp
-
-// Restore location to our patron struct
-	//pop %ecx
-
-	//cmp $0x0, %eax
-	//je ALLOCATION_FAILED
-
-// Move our pointer to allocated space for char* name
-// into byte 8 of the patron struct we allocated earlier
-	//movl %eax, 8(%ecx)
-
-	
-// Save our patron struct pointer for later use
-	//pushl %ecx
-
-	//movl 16(%ebp), %edx
-	//pushl 16(%ebp)
-	//pushl %edx
-	//pushl 8(%ecx)
-	//call strcpy
-
-	//add $8, %esp
-
-	//pop %ecx
-
+// Make byte 8 of our patron struct point to same char
+// as supplied 3rd arg char* name
 	movl 16(%ebp), %edx
 	movl %edx, 8(%ecx)
 
